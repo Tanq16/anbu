c4e33ab19a1b84201cd1df879816145bfc68ba21
Tanq16 <tanishqrupaal@Tanishqs-MacBook-Pro.local>
2025-04-12 18:15:31 -0400

    table formatting, output styles, testing

---
+++ cmd/tester.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tanq16/anbu/utils"
+)
+
+var tableTestCmd = &cobra.Command{
+	Use:   "testcommand",
+	Short: "command to test functions",
+	Run: func(cmd *cobra.Command, args []string) {
+		table := utils.MarkdownTable{
+			Headers: []string{"Component", "Status", "Version", "Last Updated"},
+			Rows: [][]string{
+				{"Operating System", "Running", "Linux 6.5", "2025-03-15"},
+				{"Database Wrap around the name", "Active", "PostgreSQL 16.2", "2025-04-01"},
+				{"Web Server", "Running", "Nginx 1.28.0", "2025-03-22"},
+				{"API Gateway", "Warning", "Kong 3.4.2", "2025-02-10"},
+				{"Cache", "Inactive", "Redis 8.0.1", "2025-01-30"},
+			},
+		}
+		err := table.OutMDPrint()
+		if err != nil {
+			utils.OutError("Failed to print table: " + err.Error())
+		} else {
+			utils.OutSuccess("Table printed successfully")
+		}
+		// err = table.OutMDFile("test.md")
+		// if err != nil {
+		// 	utils.OutError("Failed to write table to file: " + err.Error())
+		// } else {
+		// 	utils.OutSuccess("Table written to test.md")
+		// }
+
+		utils.OutWarning("Warning: This is a test warning message")
+		utils.OutError("Error: This is a test error message")
+		utils.OutSuccess("Success: This is a test success message")
+		utils.OutInfo("Info: This is a test info message")
+		utils.OutDebug("Debug: This is a test debug message")
+		fmt.Println("Test message with no color")
+		fmt.Println("Test message with no color")
+		fmt.Println("Test message with no color")
+		fmt.Println("Test message with no color")
+		utils.OutClearLines(3)
+
+		logger := utils.GetLogger("test")
+		logger.Debug().Msg("This is a debug message")
+		logger.Info().Msg("This is an info message")
+		logger.Warn().Msg("This is a warning message")
+		logger.Error().Msg("This is an error message")
+		logger.Fatal().Msg("This is a fatal message")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tableTestCmd)
+}
--- utils/output.go
+++ utils/output.go
@@ -2,8 +2,6 @@ package utils
 
 import (
 	"fmt"
-	"os"
-	"strings"
 )
 
 var OutColors = map[string]string{
@@ -72,76 +70,4 @@ func OutInfo(msg string) {
 
 func OutDebug(msg string) {
 	fmt.Printf("%s%s%s\n", OutColors["grey"], msg, OutColors["reset"])
-}
-
-type MarkdownTable struct {
-	Caption string
-	Headers []string
-	Rows    [][]string
-}
-
-// OutMDFile writes a markdown table to a file
-func (table MarkdownTable) OutMDFile(outputFile *os.File) error {
-	formatted, err := formatMDTable(table)
-	if err != nil {
-		return fmt.Errorf("error formatting table: %w", err)
-	}
-	_, err = outputFile.WriteString(formatted)
-	if err != nil {
-		return fmt.Errorf("error writing to file: %w", err)
-	}
-	return nil
-}
-
-// OutMDPrint prints a markdown table to console
-func (table MarkdownTable) OutMDPrint() error {
-	formatted, err := formatMDTable(table)
-	if err != nil {
-		return fmt.Errorf("error formatting table: %w", err)
-	}
-	fmt.Print(formatted)
-	return nil
-}
-
-func formatMDTable(table MarkdownTable) (string, error) {
-	var output strings.Builder
-	if table.Caption != "" {
-		output.WriteString("# " + table.Caption + "\n\n")
-	}
-	// Dynamically determine column width
-	colWidths := make([]int, len(table.Headers))
-	for i, header := range table.Headers {
-		colWidths[i] = len(header)
-	}
-	for _, row := range table.Rows {
-		for i, cell := range row {
-			if len(cell) > colWidths[i] {
-				colWidths[i] = len(cell)
-			}
-		}
-	}
-	// Write header
-	headerRow := "|"
-	dividerRow := "|"
-	for i, header := range table.Headers {
-		formatter := fmt.Sprintf(" %%-%ds |", colWidths[i])
-		headerRow += fmt.Sprintf(formatter, header)
-		dividerRow += fmt.Sprintf(" %s |", strings.Repeat("-", colWidths[i]))
-	}
-	headerRow += "\n"
-	dividerRow += "\n"
-	output.WriteString(headerRow)
-	output.WriteString(dividerRow)
-	// Write rows
-	for _, row := range table.Rows {
-		rowText := "|"
-		for i, cell := range row {
-			formatter := fmt.Sprintf(" %%-%ds |", colWidths[i])
-			rowText += fmt.Sprintf(formatter, cell)
-		}
-		rowText += "\n"
-		output.WriteString(rowText)
-	}
-	output.WriteString("\n\n")
-	return output.String(), nil
 }
