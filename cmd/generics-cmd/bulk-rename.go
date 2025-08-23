7cf75cbc7abf62e76469bba05d420fde2e06ba8e
Tanq16 <37408906+Tanq16@users.noreply.github.com>
2025-08-23 21:46:55 +0000

    readme nit

--- cmd/generics-cmd/bulk-rename.go
+++ cmd/generics-cmd/bulk-rename.go
@@ -17,21 +17,21 @@ var BulkRenameCmd = &cobra.Command{
 	Long: `Renames multiple files or directories in the current folder based on a regex pattern.
 It uses capture groups from the pattern in the replacement string.
 
-- Use \\1, \\2, etc. in the <replacement> string to refer to capture groups from the <pattern>.
+- Use \1, \2, etc. in the <replacement> string to refer to capture groups from the <pattern>.
 - The pattern is standard Go regex. Remember to quote arguments to prevent shell expansion.
 
 Examples:
   # Add a prefix 'new_' to files starting with 'prefix_'
-  anbu rename 'prefix_(.*)' 'new_\\1'
+  anbu rename 'prefix_(.*)' 'new_\1'
 
   # Do the same for directories instead of files
-  anbu rename 'old_(.*)' 'new_\\1' -d
+  anbu rename 'old_(.*)' 'new_\1' -d
 
   # Add '_backup' before the file extension (e.g., file.txt -> file_backup.txt)
-  anbu rename '(.*)\\.(.*)' '\\1_backup.\\2'
+  anbu rename '(.*)\.(.*)' '\1_backup.\2'
 
   # Simulate a rename operation without making changes
-  anbu rename 'image-(\\d+).jpg' 'IMG_\\1.jpeg' -r`,
+  anbu rename 'image-(\d+).jpg' 'IMG_\1.jpeg' -r`,
 	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		anbuGenerics.BulkRename(args[0], args[1], bulkRenameFlags.renameDirectories, bulkRenameFlags.dryRun)
