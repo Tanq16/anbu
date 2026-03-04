4e070864a03391a4a0016dc016c2b709ea8ee354
Tanq16 <37408906+Tanq16@users.noreply.github.com>
2026-03-04 15:21:56 -0500

    fix key-pair --ssh flag: swap inverted boolean logic so -s generates SSH keys

--- cmd/crypto-cmd/key-pair.go
+++ cmd/crypto-cmd/key-pair.go
@@ -13,7 +13,7 @@ var keyPairFlags struct {
 	outputPath  string
 	pass        string
 	keySize     int
-	noSSHFormat bool
+	sshFormat bool
 }
 
 var KeyPairCmd = &cobra.Command{
@@ -25,19 +25,19 @@ var KeyPairCmd = &cobra.Command{
 		keyDir := filepath.Dir(keyPairFlags.outputPath)
 		var result *anbuCrypto.KeyPairResult
 		var err error
-		if keyPairFlags.noSSHFormat {
-			result, err = anbuCrypto.GenerateKeyPair(keyDir, keyName, keyPairFlags.keySize)
-		} else {
+		if keyPairFlags.sshFormat {
 			result, err = anbuCrypto.GenerateSSHKeyPair(keyDir, keyName, keyPairFlags.keySize)
+		} else {
+			result, err = anbuCrypto.GenerateKeyPair(keyDir, keyName, keyPairFlags.keySize)
 		}
 		if err != nil {
 			u.PrintFatal("key pair generation failed", err)
 		}
 		u.LineBreak()
-		if keyPairFlags.noSSHFormat {
-			u.PrintGeneric(fmt.Sprintf("RSA key pair (%d bits) generated", result.KeySize))
-		} else {
+		if keyPairFlags.sshFormat {
 			u.PrintGeneric(fmt.Sprintf("SSH key pair (%d bits) generated", result.KeySize))
+		} else {
+			u.PrintGeneric(fmt.Sprintf("RSA key pair (%d bits) generated", result.KeySize))
 		}
 		u.PrintGeneric(fmt.Sprintf("Public key: %s", u.FInfo(result.PublicKeyPath)))
 		u.PrintGeneric(fmt.Sprintf("Private key: %s", u.FInfo(result.PrivateKeyPath)))
@@ -47,5 +47,5 @@ var KeyPairCmd = &cobra.Command{
 func init() {
 	KeyPairCmd.Flags().StringVarP(&keyPairFlags.outputPath, "output-path", "o", "./anbu-key", "Output path and name for the key files")
 	KeyPairCmd.Flags().IntVarP(&keyPairFlags.keySize, "key-size", "k", 2048, "RSA key size (e.g., 2048, 3072, 4096)")
-	KeyPairCmd.Flags().BoolVarP(&keyPairFlags.noSSHFormat, "ssh", "s", false, "Generate keys in SSH format instead of PEM")
+	KeyPairCmd.Flags().BoolVarP(&keyPairFlags.sshFormat, "ssh", "s", false, "Generate keys in SSH format instead of PEM")
 }
